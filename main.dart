fdbb570989e220515eca5ef7aaa351a9213825f8
Abdülselam <150773066+Selam47@users.noreply.github.com>
2024-03-11 00:20:30 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,28 @@
+import 'customer.dart';
+import 'customerType.dart';
+import 'product.dart';
+import 'user.dart';
+
+void main() {
+  Product product = new Product(name: "Toyota", unitPrice: 500.000);
+  product.displayInfo();
+
+  Product product2 = new Product.empty();
+  product2.name = "Nissan";
+  product2.displayInfo();
+
+  Product product3 =
+      new Product.all(name: "Bilgisayar", stock: 10, unitPrice: 100);
+  product3.displayInfo();
+  
+  User user = User(email: "selamk@kodlama.io", name: "Selam", lastName: "Kaya");
+  user.displayInfo();
+
+  Customer customer = Customer(
+      email: "engin@kodlama.io",
+      name: "Engin",
+      lastName: "Demiroğ",
+      customerNo: "153",
+      customerType: CustomerType.Indiviual);
+  customer.displayInfo();
+}
